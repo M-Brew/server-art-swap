361837c929ddeb2a5f10b07be58efdb93338cdee
Michael Brew <themichaelbrew@gmail.com>
2022-07-28 10:46:36 +0000

    added /api prefix to uploads

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
-    PORT = 5001,
-    MONGO_URI = "mongodb://localhost/ART_SWAP_DB"
-} = process.env;
+const { PORT = 5001, MONGO_URI = "mongodb://localhost/ART_SWAP_DB" } =
+    process.env;
 
 const app = express();
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/uploads", express.static(path.join(__dirname, "uploads")));
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
 // routes
-app.use('/api/auth', require("./routes/authRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 app.use("/api/categories", require("./routes/categoryRoutes"));
 app.use("/api/art-pieces", require("./routes/artPieceRoutes"));
